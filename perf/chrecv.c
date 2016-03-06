dba62f975b9b127c21446eb5f2ce828f8133dad5
Martin Sustrik <sustrik@250bpm.com>
2016-03-06 22:20:39 +0100

    Rename perf tests to match function names
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- perf/chrecv.c
+++ perf/chrecv.c
@@ -1,6 +1,6 @@
 /*
 
-  Copyright (c) 2015 Martin Sustrik
+  Copyright (c) 2016 Martin Sustrik
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"),
@@ -32,22 +32,27 @@
 
 int main(int argc, char *argv[]) {
     if(argc != 2) {
-        printf("usage: chr <millions-of-messages>\n");
+        printf("usage: chrecv <millions-of-messages>\n");
         return 1;
     }
     long count = atol(argv[1]) * 1000000;
 
     int ch = channel(sizeof(char), count);
+    assert(ch >= 0);
 
     long i;
     char val = 0;
-    for(i = 0; i != count; ++i)
-        chsend(ch, &val, sizeof(char), -1);
+    for(i = 0; i != count; ++i) {
+        int rc = chsend(ch, &val, sizeof(char), -1);
+        assert(rc == 0);
+    }
 
     int64_t start = now();
 
-    for(i = 0; i != count; ++i)
-        chrecv(ch, &val, sizeof(char), -1);
+    for(i = 0; i != count; ++i) {
+        int rc = chrecv(ch, &val, sizeof(char), -1);
+        assert(rc == 0);
+    }
 
     int64_t stop = now();
     long duration = (long)(stop - start);
@@ -61,4 +66,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
-
+  
