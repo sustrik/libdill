28ab2f897ae64131194827fff07841067d6f7f66
Martin Sustrik <sustrik@250bpm.com>
2016-05-14 04:07:37 +0200

    now() moved to libdill.c
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- libdill.c
+++ libdill.c
@@ -20,9 +20,39 @@
 
 */
 
+#include <stdint.h>
+#include <sys/time.h>
+#include <time.h>
+
+#if defined __APPLE__
+#include <mach/mach_time.h>
+#endif
+
 #include "cr.h"
 #include "libdill.h"
 #include "utils.h"
+
+int64_t now(void) {
+#if defined __APPLE__
+    static mach_timebase_info_data_t dill_mtid = {0};
+    if (dill_slow(!dill_mtid.denom))
+        mach_timebase_info(&dill_mtid);
+    uint64_t ticks = mach_absolute_time();
+    return (int64_t)(ticks * dill_mtid.numer / dill_mtid.denom / 1000000);
+#elif defined CLOCK_MONOTONIC
+    struct timespec ts;
+    int rc = clock_gettime(CLOCK_MONOTONIC, &ts);
+    dill_assert (rc == 0);
+    return ((int64_t)ts.tv_sec) * 1000 + (((int64_t)ts.tv_nsec) / 1000000);
+#else
+    /* This is slow and error-prone (time can jump backwards!) but it's just
+       a last resort option. */
+    struct timeval tv;
+    int rc = gettimeofday(&tv, NULL);
+    assert(rc == 0);
+    return ((int64_t)tv.tv_sec) * 1000 + (((int64_t)tv.tv_usec) / 1000);
+#endif
+}
 
 int msleep(int64_t deadline) {
     /* Return ECANCELED if shutting down. */
