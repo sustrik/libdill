9b784968b5b290bd0690e023b9eb2713b62c9316
Martin Sustrik <sustrik@250bpm.com>
2016-11-27 15:43:45 +0100

    Bump ABI version
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- libdill.h
+++ libdill.h
@@ -42,13 +42,13 @@
 /*  www.gnu.org/software/libtool/manual/html_node/Updating-version-info.html  */
 
 /*  The current interface version. */
-#define DILL_VERSION_CURRENT 7
+#define DILL_VERSION_CURRENT 8
 
 /*  The latest revision of the current interface. */
 #define DILL_VERSION_REVISION 0
 
 /*  How many past interface versions are still supported. */
-#define DILL_VERSION_AGE 1
+#define DILL_VERSION_AGE 0
 
 /******************************************************************************/
 /*  Symbol visibility                                                         */
