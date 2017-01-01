ba40858408c7ae24a1bcbed90b0f19d0b742012d
Bent Cardan <bent@nothingsatisfies.com>
2017-01-01 10:55:05 -0800

    use dill_assert

--- libdill.c
+++ libdill.c
@@ -50,7 +50,7 @@ int64_t now(void) {
        a last resort option. */
     struct timeval tv;
     int rc = gettimeofday(&tv, NULL);
-    assert(rc == 0);
+    dill_assert (rc == 0);
     return ((int64_t)tv.tv_sec) * 1000 + (((int64_t)tv.tv_usec) / 1000);
 #endif
 }
