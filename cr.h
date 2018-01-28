df14e454db37b78bd5467751cfbf4291c2944de4
Martin Sustrik <sustrik@250bpm.com>
2018-01-28 03:51:21 +0100

    align
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- cr.h
+++ cr.h
@@ -84,10 +84,13 @@ struct dill_cr {
     struct dill_census_item *census;
     size_t stacksz;
 #endif
+}
+#if !defined _MSC_VER
 /* Clang assumes that the client stack is aligned to 16-bytes on x86-64
    architectures. To achieve this, we align this structure (with the added
    benefit of a minor optimization). */
-} __attribute__((aligned(16)));
+__attribute__((aligned(16)));
+#endif
 
 struct dill_ctx_cr {
     /* Currently running coroutine. */
