3c78714d0acf4b4ea29e4f3a1fe2bee95145f6e7
Martin Sustrik <sustrik@250bpm.com>
2018-03-01 22:24:44 +0100

    man directory now contains .3 files, no .md files
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ man/generate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Use this script to regenerate the man pages.
+
+rm -f *.3
+rm -f *.md
+./generatemd.js
+for f in *.md
+do
+    echo "Processing $f"
+	  pandoc -s -f markdown_github -t man $f -o "$(basename "$f" .md).3" \
+		    -M header="libdill Library Functions" \
+		    -M footer="libdill" \
+		    -M title="`echo "$(basename "$f" .md)(3)" | tr 'a-z' 'A-Z'`"
+done
+rm -f *.md
